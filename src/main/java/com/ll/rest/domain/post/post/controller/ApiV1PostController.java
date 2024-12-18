d67e6b932c0b0a2d8b664595002ac378463c56a9
baekminjin <bhs3392@naver.com>
2024-12-18 14:33:24 +0900

    rsData

--- src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/ll/rest/domain/post/post/controller/ApiV1PostController.java
@@ -5,7 +5,9 @@ import com.ll.rest.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -25,6 +27,7 @@ public class ApiV1PostController {
 		return postService.findById(id).get();
 	}
 
+	/*
 	@DeleteMapping("/{id}")
 	public String deleteItem(
 			@PathVariable long id
@@ -35,4 +38,22 @@ public class ApiV1PostController {
 
 		return "삭제가 완료되었습니다.";
 	}
+
+	 */
+
+	@DeleteMapping("/{id}")
+	public Map<String, Object> deleteItem(
+			@PathVariable long id
+	) {
+		Post post = postService.findById(id).get();
+
+		postService.delete(post);
+
+		Map<String, Object> rsData = new HashMap<>();
+		rsData.put("resultCode", "200-1");
+		rsData.put("msg", "%d번 글을 삭제하였습니다.".formatted(id));
+
+		return rsData;
+	}
+
 }
