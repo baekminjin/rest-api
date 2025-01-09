0f7b5c35fda13c2e9e61580e51483d34c92b0fd8
baekminjin <bhs3392@naver.com>
2025-01-09 11:24:16 +0900

    condition 201

--- src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
+++ src/main/java/com/ll/rest/global/aspect/ResponseAspect.java
@@ -1,15 +1,18 @@
 package com.ll.rest.global.aspect;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
 
+
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class ResponseAspect {
+    private final HttpServletResponse response;
     @Around("""
             (
                 within
@@ -25,18 +28,17 @@ public class ResponseAspect {
                     @annotation(org.springframework.web.bind.annotation.PutMapping)
                     ||
                     @annotation(org.springframework.web.bind.annotation.DeleteMapping)
+                    ||
+                    @annotation(org.springframework.web.bind.annotation.RequestMapping)
                 )
             )
             ||
             @annotation(org.springframework.web.bind.annotation.ResponseBody)
             """)
     public Object handleResponse(ProceedingJoinPoint joinPoint) throws Throwable {
-        System.out.println("handleResponse 시작");
-
         //위의 조건 알맞는 경우에 실행
         Object proceed = joinPoint.proceed();
-
-        System.out.println("handleResponse 끝");
+        response.setStatus(201);
 
         return proceed;
     }
